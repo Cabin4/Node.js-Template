c1bb4b9746d2cc0ed7f17832aa8eb0fd9b19fc7f
ShahalKP <info4shahal@gmail.com>
2022-02-07 20:05:34 +0530

    feat: express-mongo-sanitize & cors setup

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-const express = require("express");
-const morgan = require("morgan");
-require("dotenv").config();
+const express = require('express');
+const morgan = require('morgan');
+const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
-app.use(morgan("dev"));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  })
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
 
 // Ping
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   try {
-    res.status(200).send("Ping");
+    res.status(200).send('Ping');
   } catch (err) {
     res.status(500).send(err);
   }
 });
 
-app.use("/users", require("./controllers/user.controller"));
+app.use('/users', require('./controllers/user.controller'));
